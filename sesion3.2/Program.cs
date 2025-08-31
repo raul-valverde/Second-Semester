8e98bb1c4e1be9deb27c90002720ff3b9574c0aa
raul-valverde <raulvruiz@uamv.edu.ni>
2025-08-30 18:54:40 -0600

    Add files via upload

---
+++ sesion3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Crea un programa que lea numeros ingresados y muestre cual es el mayor y el menor. El programa finaliza cuando se ingresa un -99 con while.
+int numero;
+int mayor = int.MinValue;
+int menor = int.MaxValue;
+do
+{
+    console.WriteLine("Ingrese un numero (-99 para finalizar): ");
+    numero = convert.toint32(console.readline());
+    if (numero != -99)
+    {
+        if (numero > mayor)
+        {
+            mayor = numero;
+        }
+        if (numero < menor)
+        {
+            menor = numero;
+        }
+    }
+} while (numero != -99);
+console.writeline("El numero mayor es: " + mayor);
+console.writeline("El numero menor es: " + menor);
