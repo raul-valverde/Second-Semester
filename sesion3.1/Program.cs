48379dc43c7db6c0453f22fb4378741a53cc3284
raul-valverde <raulvruiz@uamv.edu.ni>
2025-08-30 18:54:19 -0600

    Add files via upload

---
+++ sesion3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Crea un programa que sume los numeros pares e impares del 1 al 100 y muestre los resultados en pantalla.
+int i, sumaPares = 0, sumaImpares = 0;
+for (i = 1; i <= 100; i + 2)
+{
+    sumaImpares = sumaImpares + i;
+}
+Console.WriteLine("La suma de los numeros impares es: " + sumaImpares);
+for (i = 2; i <= 100; i + 2)
+{
+    sumaPares = sumaPares + i;
+}
+Console.WriteLine("La suma de los numeros pares es: " + sumaPares);
