f4fc50a2cfcfd37fb0b1f449eb5ba8fc003b11b1
raul-valverde <raulvruiz@uamv.edu.ni>
2025-08-30 19:00:12 -0600

    Add files via upload

---
+++ sesion1.2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Programa simple para calcular el tiempo de viaje en bus de Managua a Granada
+// Distancia fija: 40 km
+
+Console.WriteLine("Ingrese la velocidad promedio en km/h:");
+int velocidad = int.Parse(Console.ReadLine() ?? "0");
+int distancia = 40;
+double tiempo = distancia / velocidad;
+Console.WriteLine($"El tiempo total de viaje es: {tiempo} horas");
