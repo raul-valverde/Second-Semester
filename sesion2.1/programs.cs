5f1cf084827adad5b0e931106291e3adf1dae0fd
raul-valverde <raulvruiz@uamv.edu.ni>
2025-08-30 18:58:12 -0600

    Add files via upload

---
+++ sesion2.1/programs.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+
+int num;
+Console.WriteLine("Programa para saber si un numero es positivo, negativo o cero");
+Console.WriteLine("Ingresa un numero:");
+num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+{
+    Console.WriteLine("Numero positivo");
+}
+else
+{
+    Console.WriteLine("El numero es negativo");
+}
---
+++ sesion2.2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Programa de la semana");
+Console.WriteLine("Ingrese un numero del 1 al 7: ");
+string? entrada = Console.ReadLine();
+if (!int.TryParse(entrada, out int dia))
+{
+    Console.WriteLine("Entrada no válida. Debe ingresar un número.");
+    return;
+}
+switch (dia)
+{
+    case 1:
+        Console.WriteLine("Lunes");
+        break;
+    case 2:
+        Console.WriteLine("Martes");
+        break;
+    case 3:
+        Console.WriteLine("Miercoles");
+        break;
+    case 4:
+        Console.WriteLine("Jueves");
+        break;
+    case 5:
+        Console.WriteLine("Viernes");
+        break;
+    case 6:
+        Console.WriteLine("Sabado");
+        break;
+    case 7:
+        Console.WriteLine("Domingo");
+        break;
+    default:
+        Console.WriteLine("Numero ivalido");
+        break;
+}
